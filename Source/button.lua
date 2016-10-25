0592075e3e17066315387ddee28402ac1c1ee007
Paul Merrill <pdm@pdm.me>
2016-10-24 22:36:58 -0700

    Add a Button class

---
+++ Source/button.lua
@@ -0,0 +1,60 @@
+local EventListener = require( "Source.eventListener" )
+
+local Button = {}
+
+-- Arguments: parentGroup, font, fontSize, fontColor, text, x, y,
+--            paddingX, paddingY, fillColor, borderWidth, borderColor
+function Button:new( options )
+	-- b inherits from Button
+	local b = {}
+	setmetatable( b, self )
+	self.__index = self
+
+	b.textGroup = display.newGroup()
+	b.text = display.newText( b.textGroup, options.text, options.x, options.y, options.font, options.fontSize )
+	b.text:setFillColor( unpack( options.fontColor ) )
+
+	textWidth = b.text.width
+	textHeight = b.text.height
+
+	b.bgGroup = display.newGroup()
+	b.bg = display.newRect(
+	 	b.bgGroup,
+		options.x,
+		options.y,
+		textWidth + 2*options.paddingX + 2*options.borderWidth,
+		textHeight + 2*options.paddingY + 2*options.borderWidth
+	)
+
+	b.bg.strokeWidth = options.borderWidth
+	b.bg:setStrokeColor( unpack( options.borderColor ) )
+	b.bg:setFillColor( unpack( options.fillColor ) )
+
+	b.group = display.newGroup()
+	b.group:insert( b.bgGroup )
+	b.group:insert( b.textGroup )
+
+	options.parentGroup:insert( b.group )
+
+	b.listener = EventListener:new()
+
+	b.text:addEventListener( "tap", function() b:onTap() end )
+	b.bg:addEventListener( "tap", function() b:onTap() end )
+
+	return b
+end
+
+function Button:onTap()
+	self.listener:dispatchEvent( "press", nil )
+end
+
+function Button:addEventListener( eventName, handlerFn )
+	self.listener:addEventListener( eventName, handlerFn )
+end
+
+function Button:removeSelf()
+	self.group:removeSelf()
+	self.listener:clear()
+end
+
+return Button
