06af76e339542bbf34945a359a27e51c3e79ba02
Paul Merrill <pdm@pdm.me>
2016-10-30 16:57:34 -0700

    Menu: Refactor to be more object oriented

--- Source/menu.lua
+++ Source/menu.lua
@@ -1,4 +1,3 @@
-
 local composer = require( "composer" )
 local physics = require ( "physics")
 
@@ -13,11 +12,11 @@ local util = require( "Source.util" )
 physics.start()
 
 local scene = composer.newScene()
-local loopTimer
-local planesArray = {}
-local eventsArray = {}
--- event triggering
-local counter = 0
+
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
 
 images:defineImage( "Logo",  "Menu/MenuLogoV1Edit.png", 323, 319 )
 images:defineImage( "Logo Pressed", "Menu/MenuLogoV1Edit-pressed.png", 323, 319 )
@@ -29,25 +28,9 @@ images:defineImage( "Plane 5" , "Menu/plane5.png" , 25, 25)
 images:defineImage( "Santa" , "Menu/santa.png", 90 ,30)
 images:defineImage( "Pizza", "Menu/pizza.png", 90,90)
 
--- -----------------------------------------------------------------------------------
--- Code outside of the scene event functions below will only be executed ONCE unless
--- the scene is removed entirely (not recycled) via "composer.removeScene()"
--- -----------------------------------------------------------------------------------
-
-local menuMusicChannel
-
-local function removeMinigames()
-	composer.removeScene( "Source.flagGame" )
-	composer.removeScene( "Source.foodGame" )
-end
-
 sounds:defineSound( "Charm", "Assets/Sounds/Menu/Charm.mp3", 1.0 )
-musics:defineMusic( "Menu Theme", "Assets/Sounds/Music/bensound-littleidea.mp3", 0.7, 5000 )
 
-local function startMusic()
-	-- This music will be turned off in difficultySelector.lua
-	menuMusicChannel = musics:play( "Menu Theme" )
-end
+musics:defineMusic( "Menu Theme", "Assets/Sounds/Music/bensound-littleidea.mp3", 0.7, 5000 )
 
 
 -- -----------------------------------------------------------------------------------
@@ -73,19 +56,34 @@ function scene:create( event )
 
 	local sceneGroup = self.view
 
+	self.planesArray = {}
+	self.eventsArray = {}
+	-- event triggering
+	self.counter = 0
+
+	local bgGroup = display.newGroup()
+	local doodadsGroup = display.newGroup()
+	local uiGroup = display.newGroup()
+
+	self.doodadsGroup = doodadsGroup
+
+	sceneGroup:insert(bgGroup)
+	sceneGroup:insert(doodadsGroup)
+	sceneGroup:insert(uiGroup)
+
 	----------------
 	-- Background --
 	----------------
 
 	local bgWhiteFill = display.newRect(
-		sceneGroup,
+		bgGroup,
 		display.contentCenterX, display.contentCenterY,
 		display.contentWidth, display.contentHeight
 	)
 	bgWhiteFill:setFillColor( 1, 1, 1 )
 
 	local bgWorldMap = display.newImageRect(
-		sceneGroup,
+		bgGroup,
 		"Assets/Images/Menu/MenuBackgroundV1Edit.png",
 		display.contentWidth, display.contentHeight*1.3
 	)
@@ -94,7 +92,7 @@ function scene:create( event )
 	bgWorldMap.alpha = 0.5
 
 	self.logo = Button:newImageButton{
-		group = sceneGroup,
+		group = uiGroup,
 		image = images:get( sceneGroup, "Logo" ),
 		imagePressed = images:get( sceneGroup, "Logo Pressed" ),
 		x = display.contentCenterX,
@@ -108,105 +106,7 @@ function scene:create( event )
 	---------------------------
 	physics.pause()
 
-	local function createPlanes()
-
-		local plane1 = images:get( sceneGroup, "Plane 1")
-		plane1:rotate(90)
-		plane1.x = 100
-		plane1.y = 200
-		plane1.alpha = 0.6
-		physics.addBody(plane1, "dynamic", { radius = 30, bounce = 0.5})
-		plane1:setLinearVelocity(50,0)
-
-		local plane2 = images:get( sceneGroup, "Plane 2")
-		plane2:rotate(-90)
-		plane2.x = display.contentWidth - 50
-		plane2.y = 300
-		plane2.alpha = 0.6
-		physics.addBody(plane2, "dynamic", { radius = 30, bounce = 0.5})
-		plane2:setLinearVelocity(-70,0)
-
-		local plane3 = images:get( sceneGroup, "Plane 3")
-		plane3:rotate(90)
-		plane3.x = 100
-		plane3.y = 400
-		plane3.alpha = 0.6
-		physics.addBody(plane3, "dynamic", { radius = 30, bounce = 0.5})
-		plane3:setLinearVelocity(80,0)
-
-		local plane4 = images:get( sceneGroup, "Plane 4")
-		plane4:rotate(-90)
-		plane4.x = display.contentWidth - 100
-		plane4.y = 500
-		plane4.alpha = 0.6
-		physics.addBody(plane4, "dynamic", { radius = 30, bounce = 0.5})
-		plane4:setLinearVelocity(-60,0)
-
-		local plane5 = images:get( sceneGroup, "Plane 5")
-		plane5:rotate(90)
-		plane5.x = 100
-		plane5.y = 600
-		plane5.alpha = 0.6
-		physics.addBody(plane5, "dynamic", { radius = 30, bounce = 0.5})
-		plane5:setLinearVelocity(40,0)
-
-		table.insert(planesArray,plane1)
-		table.insert(planesArray,plane2)
-		table.insert(planesArray,plane3)
-		table.insert(planesArray,plane4)
-		table.insert(planesArray,plane5)
-	end
-
-	-- function generates planes
-	function looping()
-		-- create new planes
-		createPlanes()
-
-		-- Remove out of screen planes
-		for i = #planesArray, 1, -1 do
-			local thisPlane = planesArray[i]
-
-			if ( thisPlane.x < 100 or thisPlane.x > display.contentWidth + 100) then
-				display.remove( thisPlane )
-				table.remove(planesArray,i)
-			end
-		end
-	end
-	looping()
-
-	-- function handles events triggering
-	function eventsTrigger()
-		for i = #eventsArray, 1, -1 do
-			local thisEvent = eventsArray[i]
-
-			if ( thisEvent.x < 100 or thisEvent.x > display.contentWidth + 100) then
-				display.remove( thisEvent )
-				table.remove(eventsArray,i)
-			end
-		end
-		-- trigger santa clause
-		if counter == 2 then
-			local santa = images:get( sceneGroup, "Santa")
-			santa:rotate(20)
-			santa.x = math.random(0,300)
-			santa.y = 0
-			santa.alpha = 0.8
-			physics.addBody(santa, "kinematic", { radius = 30, bounce = 0.8})
-			santa:setLinearVelocity(math.random(50,100),math.random(50,100))
-			table.insert(eventsArray,santa)
-		-- trigger giant pizza
-		elseif counter == 5 then
-			local pizza = images:get(sceneGroup, "Pizza")
-			pizza.x = 0
-			pizza.y = math.random(0,300)
-			pizza.alpha = 0.8
-			physics.addBody(pizza, "dynamic", {radius = 30, bounce = 0.8})
-			pizza:setLinearVelocity(math.random(100,300),math.random(100, 150))
-			pizza:applyTorque(math.random(-4,4))
-			table.insert(eventsArray,pizza)
-		end
-
-	end
+	self:createPlanes()
 
 	----------------
 	-- Foreground --
@@ -217,7 +117,7 @@ function scene:create( event )
 	local titleFontSize = (util.aspectRatio() > 4/3 and 110 or 140)
 
 	titleText = display.newText(
-		sceneGroup,
+		uiGroup,
 		"Culture Fun",
 		display.contentCenterX,
 		titleOffsetY,
@@ -227,7 +127,7 @@ function scene:create( event )
 	titleText:setFillColor( 0.4, 0.4, 0.4 )
 
 	self.flagButton = Button:newTextButton{
-		group=sceneGroup,
+		group=uiGroup,
 		font=font, fontSize=44, fontColor={ 0.4 },
 		text="Play Flag Game",
 		x=200, y=display.contentCenterY + 50,
@@ -236,7 +136,7 @@ function scene:create( event )
 		borderWidth=3, borderColor={ 0.85 }
 	}
 	self.foodButton = Button:newTextButton{
-		group=sceneGroup,
+		group=uiGroup,
 		font=font, fontSize=44, fontColor={ 0.4 },
 		text="Play Food Game",
 		x=display.contentWidth - 200, y=display.contentCenterY + 50,
@@ -246,7 +146,7 @@ function scene:create( event )
 	}
 
 	self.creditButton = Button:newTextButton{
-		group=sceneGroup,
+		group=uiGroup,
 		font=font, fontSize=20, fontColor={ 0.4 },
 		text="Credit",
 		x=display.contentWidth-130, y=display.contentHeight-140,
@@ -300,9 +200,12 @@ function scene:show( event )
 
 	elseif ( phase == "did" ) then
 		-- Code here runs when the scene is entirely on screen
-	    loopTimer = timer.performWithDelay(11000, function() looping() end, 0)
-		timer.performWithDelay( 25, startMusic )
-		timer.performWithDelay( 25, removeMinigames )
+	    self.loopTimer = timer.performWithDelay(11000, function()
+			self:removeOldPlanes()
+			self:createPlanes()
+		end, 0)
+		timer.performWithDelay( 25, function() self:startMusic() end )
+		timer.performWithDelay( 25, function() self:removeMinigames() end )
 		timer.performWithDelay( 25, function()
 			if self.preloader == nil then
 				self.preloader = self:preload()
@@ -322,7 +225,7 @@ function scene:hide( event )
 		-- Code here runs when the scene is on screen (but is about to go off screen)
 
 		-- this cancels the loop generating planes
-		timer.cancel(loopTimer)
+		timer.cancel(self.loopTimer)
 
 	elseif ( phase == "did" ) then
 		-- Code here runs immediately after the scene goes entirely off screen
@@ -353,7 +256,7 @@ function scene:gotoMinigame( name, file, menu )
 			sourcePath = sourcePath,
 			preloadFn = function() return minigameScene:preload() end
 		},
-		menuMusicChannel = menuMusicChannel
+		menuMusicChannel = self.menuMusicChannel
 	}
 	composer.gotoScene( "Source.difficultySelector", { params=params } )
 end
@@ -386,17 +289,12 @@ function scene:logoTapped( event )
 		return
 	end
 
-	if counter ~= 8 then
-		counter = counter + 1
-		eventsTrigger()
-	else
-		counter = 0
-	end
-
 	self:spinLogo()
 end
 
 function scene:spinLogo()
+	self:maybeSpawnFunnies()
+
 	timer.performWithDelay( 0, function() sounds:play( "Charm" ) end )
 
 	self.spinning = true
@@ -415,6 +313,119 @@ function scene:spinLogo()
 			self:spinLogo()
 		end
 	end})
+end
+
+function scene:removeOldPlanes()
+	-- Remove out of screen planes
+	for i = #self.planesArray, 1, -1 do
+		local thisPlane = self.planesArray[i]
+
+		if ( thisPlane.x < 100 or thisPlane.x > display.contentWidth + 100) then
+			display.remove( thisPlane )
+			table.remove(self.planesArray,i)
+		end
+	end
+end
+
+function scene:createPlanes()
+
+	local plane1 = images:get( self.doodadsGroup, "Plane 1")
+	plane1:rotate(90)
+	plane1.x = 100
+	plane1.y = 200
+	plane1.alpha = 0.6
+	physics.addBody(plane1, "dynamic", { radius = 30, bounce = 0.5})
+	plane1:setLinearVelocity(50,0)
+
+	local plane2 = images:get( self.doodadsGroup, "Plane 2")
+	plane2:rotate(-90)
+	plane2.x = display.contentWidth - 50
+	plane2.y = 300
+	plane2.alpha = 0.6
+	physics.addBody(plane2, "dynamic", { radius = 30, bounce = 0.5})
+	plane2:setLinearVelocity(-70,0)
+
+	local plane3 = images:get( self.doodadsGroup, "Plane 3")
+	plane3:rotate(90)
+	plane3.x = 100
+	plane3.y = 400
+	plane3.alpha = 0.6
+	physics.addBody(plane3, "dynamic", { radius = 30, bounce = 0.5})
+	plane3:setLinearVelocity(80,0)
+
+	local plane4 = images:get( self.doodadsGroup, "Plane 4")
+	plane4:rotate(-90)
+	plane4.x = display.contentWidth - 100
+	plane4.y = 500
+	plane4.alpha = 0.6
+	physics.addBody(plane4, "dynamic", { radius = 30, bounce = 0.5})
+	plane4:setLinearVelocity(-60,0)
+
+	local plane5 = images:get( self.doodadsGroup, "Plane 5")
+	plane5:rotate(90)
+	plane5.x = 100
+	plane5.y = 600
+	plane5.alpha = 0.6
+	physics.addBody(plane5, "dynamic", { radius = 30, bounce = 0.5})
+	plane5:setLinearVelocity(40,0)
+
+	table.insert(self.planesArray,plane1)
+	table.insert(self.planesArray,plane2)
+	table.insert(self.planesArray,plane3)
+	table.insert(self.planesArray,plane4)
+	table.insert(self.planesArray,plane5)
+end
+
+function scene:maybeSpawnFunnies()
+	if self.counter < 8 then
+		self.counter = self.counter + 1
+		spawnFunnies()
+	else
+		self.counter = 0
+	end
+end
+
+function scene:spawnFunnies()
+	for i = #self.eventsArray, 1, -1 do
+		local thisEvent = self.eventsArray[i]
+
+		if ( thisEvent.x < 100 or thisEvent.x > display.contentWidth + 100) then
+			display.remove( thisEvent )
+			table.remove(self.eventsArray,i)
+		end
+	end
+	-- trigger santa clause
+	if self.counter == 2 then
+		local santa = images:get( doodadsGroup, "Santa")
+		santa:rotate(20)
+		santa.x = math.random(0,300)
+		santa.y = 0
+		santa.alpha = 0.8
+		physics.addBody(santa, "kinematic", { radius = 30, bounce = 0.8})
+		santa:setLinearVelocity(math.random(50,100),math.random(50,100))
+		table.insert(self.eventsArray,santa)
+	-- trigger giant pizza
+	elseif self.counter == 5 then
+		local pizza = images:get(doodadsGroup, "Pizza")
+		pizza.x = 0
+		pizza.y = math.random(0,300)
+		pizza.alpha = 0.8
+		physics.addBody(pizza, "dynamic", {radius = 30, bounce = 0.8})
+		pizza:setLinearVelocity(math.random(100,300),math.random(100, 150))
+		pizza:applyTorque(math.random(-4,4))
+		table.insert(self.eventsArray,pizza)
+	end
+
+end
+
+function scene:removeMinigames()
+	composer.removeScene( "Source.flagGame" )
+	composer.removeScene( "Source.foodGame" )
+end
+
+function scene:startMusic()
+	-- This music will be turned off in difficultySelector.lua
+	self.menuMusicChannel = musics:play( "Menu Theme" )
 end
 
 -- -----------------------------------------------------------------------------------
