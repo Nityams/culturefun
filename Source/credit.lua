d9719b9d0fffcd3e0749dc49e9432736ae9221de
Paul Merrill <pdm@pdm.me>
2016-10-30 15:58:03 -0700

    Credits: Add a return button

--- Source/credit.lua
+++ Source/credit.lua
@@ -16,6 +16,8 @@ local scene = composer.newScene()
 -- -----------------------------------------------------------------------------------
 
 images:defineImage( "Credit", "Credits/credit.png", display.contentWidth, display.contentHeight-150 )
+images:defineImage( "Close Button", "Scene/11.png", display.contentWidth/20, display.contentHeight/14 )
+images:defineImage( "Close Button Pressed", "Scene/11-pressed.png", display.contentWidth/20, display.contentHeight/14 )
 musics:defineMusic( "Flag Theme", "Assets/Sounds/Music/Fate-Stay-Night.mp3", 0.8, 5000 )
 
 local function gotoGame()
@@ -53,9 +55,19 @@ function scene:create( event )
 	self.neighborhood = images:get( sceneGroup, "Credit" )
 	self.neighborhood.x = display.contentCenterY+400
 	self.neighborhood.y = display.contentCenterX+400
-	
+
 	moveImage(self.neighborhood)
 
+	Button:newImageButton{
+		group = sceneGroup,
+		image = images:get( sceneGroup, "Close Button" ),
+		imagePressed = images:get( sceneGroup, "Close Button Pressed" ),
+		x = display.contentWidth / 20,
+		y = display.contentHeight / 5,
+		width = images:width( "Close Button" ),
+		height = images:height( "Close Button" ),
+		alpha = 0.5
+	}
 end
 
 
@@ -88,7 +100,7 @@ function scene:hide( event )
 
 	if ( phase == "will" ) then
 		-- Code here runs when the scene is on screen (but is about to go off screen)
-	
+
 	elseif ( phase == "did" ) then
 		-- Code here runs immediately after the scene goes entirely off screen
 
