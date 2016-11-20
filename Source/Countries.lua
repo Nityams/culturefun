cdf8e746eaeb0115bd3bdbfa250a04d2aabf5eb4
hansenwu <hansen.wu.85@gmail.com>
2016-11-19 23:15:30 -0800

    More countries, more action, MORE AWESOME SAUCEcd Source/cd Source/1llcd Source/clear I mean, more countries added and sorted into difficulty tables, deck building function now supports difficulty levels

--- Source/Countries.lua
+++ Source/Countries.lua
@@ -1,219 +1,540 @@
-Countries = {}
-deck = {}
-
-Spain = {
+local Spain = {
   name = "Spain",
   flag = "Assets/Images/Flags/Spain_Flag.png",
   food = "Paella",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/spanish_dialog.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/spanish_dialog.png",
   greeting = "Hola",
-  coordinates = { lat = 40.43, lon = -3.70 }  -- Madrid @ 40°26′N 3°42′W
+  coordinates = { lat = 40.43, lon = -3.70 },  -- Madrid @ 40°26′N 3°42′W
+  fun_fact = {
+    "Did you know, the world’s oldest restaurant, Botin Restaurant, is located in Madrid Spain?",
+    "Madrid is the largest city and capital of Spain!"
+  }
 }
-USA = {
-  name = "USA",
+local USA = {
+  name = "United States of America",
   flag = "Assets/Images/Flags/United_States_Flag.png",
   food = "Apple Pie",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/american_dialog.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/american_dialog.png",
   greeting = "Hello",
-  coordinates = { lat = 38.88, lon = -77.02 }  -- Washington, D.C. @ 38°53′N 77°01′W
+  coordinates = { lat = 38.88, lon = -77.02 },  -- Washington, D.C. @ 38°53′N 77°01′W
+  fun_fact = {
+    "The first man to walk on the moon was the American Neil Armstrong.",
+    "The U.S. has the longest border in the world with Canada but the border is not a straight line!"
+  }
 }
-South_Africa= {
+local South_Africa= {
   name = "South Africa",
   flag = "Assets/Images/Flags/South_Africa_Flag.png",
   food = "Chakalaka pap",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/south_african_dialog.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/south_african_dialog.png",
   greeting = "Hello",
   coordinates = { lat = -26.44, lon = 28.06 }  -- Johannesburg @ 26°12′16″S 28°2′44″E
 }
-Mexico = {
+local Mexico = {
   name = "Mexico",
   flag = "Assets/Images/Flags/Mexico_Flag.png",
   food = "Tamales",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/mexican_dialog.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/mexican_dialog.png",
   greeting = "Hola",
   coordinates = { lat = 19.43, lon = -99.13 }  -- Mexico City @ 19°26′N 99°08′W
 }
-Russia = {
+local Russia = {
   name = "Russia",
   flag = "Assets/Images/Flags/Russia_Flag.png",
   food = "Borsch",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/russian_dialog.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/russian_dialog.png",
   greeting = "Zdravstvuyte",
   coordinates = { lat = 55.75, lon = 37.62 }  -- Moscow @ 55°45′N 37°37′E
 }
-Egypt = {
+local Egypt = {
   name = "Egypt",
   flag = "Assets/Images/Flags/Egypt_Flag.png",
   food = "Koshari",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/egyptian_dialog.png",
-  greeting = "Marhabaan",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/egyptian_dialog.png",
+  greeting = "Marḥabaan",
   coordinates = { lat = 30.03, lon = 31.22 }  -- Cairo @ 30°2′N 31°13′E
 }
-New_Zealand = {
+local New_Zealand = {
   name = "New Zealand",
   flag = "Assets/Images/Flags/New_Zealand_Flag.png",
   food = "Hangi",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/new-zealand_dialog.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/new-zealand_dialog.png",
   greeting = "Hello",
   coordinates = { lat = -41.28, lon = 174.45 }  -- Wellington @ 41°17′S 174°27′E
 }
-UK = {
-  name = "UK",
+local UK = {
+  name = "United Kingdom",
   flag = "Assets/Images/Flags/United_Kingdom_Flag.png",
   food = "Fish and Chips",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/british_dialog.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/british_dialog.png",
   greeting = "Hello",
   coordinates = { lat = 51.62, lon = -0.12 }  -- London @ 51°30′N 0°7′W
 }
-Switzerland = {
+local Switzerland = {
   name = "Switzerland",
   flag = "Assets/Images/Flags/Switzerland_Flag.png",
   food = "Cheese Fondue",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/swiss_dialog.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/swiss_dialog.png",
   greeting = "Bonjour",
   coordinates = { lat = 46.95, lon = 7.45 }  -- Bern @ 46°57′N 7°27′E
 }
-Vietnam = {
+local Vietnam = {
   name = "Vietnam",
   flag = "Assets/Images/Flags/Vietnam_Flag.png",
   food = "Pho",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/vietnamese_dialog.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/vietnamese_dialog.png",
   greeting = "Chao ban",
   coordinates = { lat = 21.03, lon = 105.85 }  -- Hanoi @ 21°2′N 105°51′E
 }
 
 --new items added on Nov 6th
-Austraila = {
+local Austraila = {
   name = "Austraila",
   flag = "Assets/Images/Flags/Australia_Flag.png",
   food = "Barramundi",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
   greeting = "Hello"
 }
-Brazil = {
+local Brazil = {
   name = "Brazil",
   flag = "Assets/Images/Flags/Brazil_Flag.png",
   food = "Feijoada",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
   greeting = "Olá"
 }
-Canada = {
+local Canada = {
   name = "Canada",
   flag = "Assets/Images/Flags/Canada_Flag.png",
   food = "Poutine",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
   greeting = "Hello"
 }
-China = {
+local China = {
   name = "China",
   flag = "Assets/Images/Flags/China_Flag.png",
   food = "Dumplings",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
   greeting = "Nǐ hǎo"
 }
-France = {
+local France = {
   name = "France",
   flag = "Assets/Images/Flags/France_Flag.png",
   food = "Cassoulet",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
   greeting = "Bonjour"
 }
-Germany = {
+local Germany = {
   name = "Germany",
   flag = "Assets/Images/Flags/Germany_Flag.png",
   food = "Rouladen",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
   greeting = "Hallo"
 }
-Italy = {
+local Italy = {
   name = "Italy",
   flag = "Assets/Images/Flags/Italy_Flag.png",
   food = "Pasta",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
   greeting = "Ciao"
 }
-Japan = {
+local Japan = {
   name = "Japan",
   flag = "Assets/Images/Flags/Japan_Flag.png",
   food = "Sushi",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
   greeting = "Kon'nichiwa"
 }
-Malaysia = {
+local Malaysia = {
   name = "Malaysia",
   flag = "Assets/Images/Flags/Malaysia_Flag.png",
   food = "Satay",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
   greeting = "Hello"
 }
-Korea = {
-  name = "Korea",
+local South_Korea = {
+  name = "South Korea",
   flag = "Assets/Images/Flags/South_Korea_Flag.png",
   food = "Kimchi",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
   greeting = "Yeoboseyo"
 }
-Thailand = {
+local Thailand = {
   name = "Thailand",
   flag = "Assets/Images/Flags/Thailand_Flag.png",
   food = "Som Tum",
-  greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
+  -- greetings_food = "Assets/Images/FoodGame/Dialogs/dialogBox_white.png",
   greeting = "S̄wạs̄dī"
 }
-table.insert(Countries, Spain) --Paella
-table.insert(Countries, USA)  -- Apple Pie
-table.insert(Countries, South_Africa) --Chakalaka & pap
-table.insert(Countries, Mexico) --Tamales
-table.insert(Countries, Russia) --Borsch
-table.insert(Countries, Egypt)  --Koshari
-table.insert(Countries, New_Zealand)  --Hangi
-table.insert(Countries, UK)   --Fish and Chips
-table.insert(Countries, Switzerland) --Cheese fondue
-table.insert(Countries, Vietnam)  --Pho
-table.insert( Countries, Austraila )
-table.insert( Countries, Brazil )
-table.insert( Countries, Canada )
-table.insert( Countries, China )
-table.insert( Countries, France )
-table.insert( Countries, Germany )
-table.insert( Countries, Italy )
-table.insert( Countries, Japan )
-table.insert( Countries, Malaysia )
-table.insert( Countries, Korea )
-table.insert( Countries, Thailand )
 
+-- Countries added Nov. 19
 
+local Netherlands = {
+  name = "Netherlands",
+  flag = "Assets/Images/Flags/Netherlands_Flag.png",
+  food = "Pannenkoeken",
+  greeting = "Hallo"
+}
 
--- No flags or dialog
--- table.insert(Countries, France)
--- table.insert(Countries, China)
--- table.insert(Countries, Japan)
--- table.insert(Countries, Brazil)
+local Honduras = {
+  name = "Honduras",
+  flag = "Assets/Images/Flags/Honduras_Flag.png",
+  food = "Sopa de caracol",
+  greeting = "Hola"
+}
+local Hungary = {
+  name = "Hungary",
+  flag = "Assets/Images/Flags/Hungary_Flag.png",
+  food = "Gulyás",
+  greeting = "Szia"
+}
+local Madagascar = {
+  name = "Madagascar",
+  flag = "Assets/Images/Flags/Madagascar_Flag.png",
+  food = "Laoka",
+  greeting = "Salama"
+}
+local Mongolia = {
+  name = "Mongolia",
+  flag = "Assets/Images/Flags/Mongolia_Flag.png",
+  food = "Khuushuur",
+  greeting = "Sain uu"
+}
+local Morocco = {
+  name = "Morocco",
+  flag = "Assets/Images/Flags/Morocco_Flag.png",
+  food = "Couscous",
+  greeting = "Marḥabaan"
+}
+local Peru = {
+  name = "Peru",
+  flag = "Assets/Images/Flags/Peru_Flag.png",
+  food = "Lomo Saltado",
+  greeting = "Hola"
+}
+local Philippines = {
+  name = "Philippines",
+  flag = "Assets/Images/Flags/Philippines_Flag.png",
+  food = "Lumpia",
+  greeting = "Kamusta"
+}
+local Taiwan = {
+  name = "Taiwan",
+  flag = "Assets/Images/Flags/Taiwan_Flag.png",
+  food = "Chòu Dòufu",
+  greeting = "Nǐ hǎo"
+}
+local Ukraine = {
+  name = "Ukraine",
+  flag = "Assets/Images/Flags/Ukraine_Flag.png",
+  food = "Paska",
+  greeting = "Ahov"
+}
+local Uruguay = {
+  name = "Uruguay",
+  flag = "Assets/Images/Flags/Uruguay_Flag.png",
+  food = "Asado",
+  greeting = "Hola"
+}
 
--- Print out contents of Countries and Deck tables
+local Croatia = {
+  name = "Croatia",
+  flag = "Assets/Images/Flags/Croatia_Flag.png",
+  food = "Žganci",
+  greeting = "Bok"
+}
+local Dominican_Republic = {
+  name = "Dominican Republic",
+  flag = "Assets/Images/Flags/Dominican_Republic_Flag.png",
+  food = "Sancocho",
+  greeting = "Hola"
+}
+local East_Timor = {
+  name = "East Timor",
+  flag = "Assets/Images/Flags/East_Timor_Flag.png",
+  food = "Bibinka",
+  greeting = "Olá"
+}
+local El_Salvador = {
+  name = "El Salvador",
+  flag = "Assets/Images/Flags/El_Salvador_Flag.png",
+  food = "Pupusas",
+  greeting = "Hola"
+}
+local Georgia = {
+  name = "Georgia",
+  flag = "Assets/Images/Flags/Georgia_Flag.png",
+  food = "Khachapuri",
+  greeting = "Gamarjoba"
+}
+local Guatemala = {
+  name = "Guatemala",
+  flag = "Assets/Images/Flags/Guatemala_Flag.png",
+  food = "Chiles Rellenos",
+  greeting = "Hola"
+}
+local Haiti = {
+  name = "Haiti",
+  flag = "Assets/Images/Flags/Haiti_Flag.png",
+  food = "Griyo",
+  greeting = "Hola"
+}
+local Kazakhstan = {
+  name = "Kazakhstan",
+  flag = "Assets/Images/Flags/Kazakhstan_Flag.png",
+  food = "Kuirdak",
+  greeting = "Sälem"
+}
+local Kenya = {
+  name = "Kenya",
+  flag = "Assets/Images/Flags/Kenya_Flag.png",
+  food = "Nyama Choma",
+  greeting = "Hodi"
+}
+local Laos = {
+  name = "Laos",
+  flag = "Assets/Images/Flags/Laos_Flag.png",
+  food = "Khao Poon",
+  greeting = "Sába̖aidi̖i"
+}
+local Liberia = {
+  name = "Liberia",
+  flag = "Assets/Images/Flags/Liberia_Flag.png",
+  food = "Dumboy",
+  greeting = "Hello"
+}
+local Luxembourg = {
+  name = "Luxembourg",
+  flag = "Assets/Images/Flags/Luxembourg_Flag.png",
+  food = "Quetschentaart",
+  greeting = "Hello"
+}
+local Macedonia = {
+  name = "Macedonia",
+  flag = "Assets/Images/Flags/Macedonia_Flag.png",
+  food = "Tavče gravče",
+  greeting = "Zdravo"
+}
+local Maldives = {
+  name = "Maldives",
+  flag = "Assets/Images/Flags/Maldives_Flag.png",
+  food = "Dhon Riha",
+  greeting = "Assalaamu Alaikum"
+}
+local Montenegro = {
+  name = "Montenegro",
+  flag = "Assets/Images/Flags/Montenegro_Flag.png",
+  food = "Kačamak",
+  greeting = "Zdravo"
+}
+local Mozambique = {
+  name = "Mozambique",
+  flag = "Assets/Images/Flags/Mozambique_Flag.png",
+  food = "Pãozinho",
+  greeting = "Olá"
+}
+local Nauru = {
+  name = "Nauru",
+  flag = "Assets/Images/Flags/Nauru_Flag.png",
+  food = "Pandanus",
+  greeting = "Ekamowir Omo"
+}
+local Nigeria = {
+  name = "Nigeria",
+  flag = "Assets/Images/Flags/Nigeria_Flag.png",
+  food = "Suya",
+  greeting = "Kedu"
+}
+local Norway = {
+  name = "Norway",
+  flag = "Assets/Images/Flags/Norway_Flag.png",
+  food = "Fårikål",
+  greeting = "Hallo"
+}
+local Palau = {
+  name = "Palau",
+  flag = "Assets/Images/Flags/Palau_Flag.png",
+  food = "Cassava",
+  greeting = "Hello"
+}
+local Portugal = {
+  name = "Portugal",
+  flag = "Assets/Images/Flags/Portugal_Flag.png",
+  food = "Caldo Verde",
+  greeting = "Olá"
+}
+local Saudi_Arabia = {
+  name = "Saudi Arabia",
+  flag = "Assets/Images/Flags/Saudi_Arabia_Flag.png",
+  food = "Murtabak",
+  greeting = "Marḥabaan"
+}
+local Serbia = {
+  name = "Serbia",
+  flag = "Assets/Images/Flags/Serbia_Flag.png",
+  food = "Ćevapi",
+  greeting = "Zdravo"
+}
+local Singapore = {
+  name = "Singapore",
+  flag = "Assets/Images/Flags/Singapore_Flag.png",
+  food = "Hokkien mee",
+  greeting = "Hello"
+}
+local Slovakia = {
+  name = "Slovakia",
+  flag = "Assets/Images/Flags/Slovakia_Flag.png",
+  food = "Halušky",
+  greeting = "Ahoj"
+}
+local Slovenia = {
+  name = "Slovenia",
+  flag = "Assets/Images/Flags/Slovenia_Flag.png",
+  food = "Mavželj",
+  greeting = "Zdravo"
+}
+local Sudan = {
+  name = "Sudan",
+  flag = "Assets/Images/Flags/Sudan_Flag.png",
+  food = "Goraasa",
+  greeting = "Marḥabaan"
+}
+local Syria = {
+  name = "Syria",
+  flag = "Assets/Images/Flags/Syria_Flag.png",
+  food = "Kebab",
+  greeting = "Marḥabaan"
+}
+local Vatican_City = {
+  name = "Vatican City",
+  flag = "Assets/Images/Flags/Vatican_City_Flag.png",
+  food = "Bruschetta",
+  greeting = "Ciao"
+}
+local Zimbabwe = {
+  name = "Zimbabwe",
+  flag = "Assets/Images/Flags/Zimbabwe_Flag.png",
+  food = "Sadza",
+  greeting = "Mhoro"
+}
+
+local countries_easy = {}
+local countries_medium = {}
+local countries_hard = {}
+local deck = {}
+
+table.insert(countries_easy, Australia)
+table.insert(countries_easy, Canada)
+table.insert(countries_easy, China)
+table.insert(countries_easy, France)
+table.insert(countries_easy, Germany)
+table.insert(countries_easy, Italy)
+table.insert(countries_easy, Japan)
+table.insert(countries_easy, Mexico)
+table.insert(countries_easy, Netherlands)
+table.insert(countries_easy, Russia)
+table.insert(countries_easy, South_Africa)
+table.insert(countries_easy, South_Korea)
+table.insert(countries_easy, UK)
+table.insert(countries_easy, USA)
+table.insert(countries_easy, Vietnam)
+
+table.insert(countries_medium, Brazil)
+table.insert(countries_medium, Egypt)
+table.insert(countries_medium, Honduras)
+table.insert(countries_medium, Hungary)
+table.insert(countries_medium, Madagascar)
+table.insert(countries_medium, Malaysia)
+table.insert(countries_medium, Mongolia)
+table.insert(countries_medium, Morocco)
+table.insert(countries_medium, New_Zealand)
+table.insert(countries_medium, Peru)
+table.insert(countries_medium, Philippines)
+table.insert(countries_medium, Spain)
+table.insert(countries_medium, Switzerland)
+table.insert(countries_medium, Taiwan)
+table.insert(countries_medium, Thailand)
+table.insert(countries_medium, Ukraine)
+table.insert(countries_medium, Uruguay)
+
+table.insert(countries_hard, Croatia)
+table.insert(countries_hard, Dominican_Republic)
+table.insert(countries_hard, East_Timor)
+table.insert(countries_hard, El_Salvador)
+table.insert(countries_hard, Georgia)
+table.insert(countries_hard, Guatemala)
+table.insert(countries_hard, Haiti)
+table.insert(countries_hard, Kazakhstan)
+table.insert(countries_hard, Kenya)
+table.insert(countries_hard, Laos)
+table.insert(countries_hard, Liberia)
+table.insert(countries_hard, Luxembourg)
+table.insert(countries_hard, Macedonia)
+table.insert(countries_hard, Maldives)
+table.insert(countries_hard, Montenegro)
+table.insert(countries_hard, Mozambique)
+table.insert(countries_hard, Nauru)
+table.insert(countries_hard, Nigeria)
+table.insert(countries_hard, Norway)
+table.insert(countries_hard, Palau)
+table.insert(countries_hard, Portugal)
+table.insert(countries_hard, Saudi_Arabia)
+table.insert(countries_hard, Serbia)
+table.insert(countries_hard, Singapore)
+table.insert(countries_hard, Slovakia)
+table.insert(countries_hard, Slovenia)
+table.insert(countries_hard, Sudan)
+table.insert(countries_hard, Syria)
+table.insert(countries_hard, Vatican_City)
+table.insert(countries_hard, Zimbabwe)
+
+-- Print out contents of countries_easy and Deck tables
 function showCountries()
-  for i,v in ipairs(Countries) do
+  print("easy")
+  for i,v in ipairs(countries_easy) do
+    print(i, v.name)
+  end
+  print("medium")
+  for i,v in ipairs(countries_medium) do
+    print(i, v.name)
+  end
+  print("hard")
+  for i,v in ipairs(countries_hard) do
     print(i, v.name)
   end
 end
+
 function showDeck()
   for i,v in ipairs(deck) do
     print(i, v.name)
   end
 end
+
 function showTable(t)
   for i,v in ipairs(t) do
     print(i, v.name)
   end
 end
 
--- Clear current deck and create new one from Countries
-function buildDeck()
+-- Clear current deck and build new one with desired difficulty
+function buildDeck(difficulty)
   -- Discard
   deck = {}
+
   -- Build
-  for i,v in ipairs(Countries) do
-    deck[i] = v
+  -- Easy
+  if difficulty > 0 then
+    print("Adding easy countries")
+    for k,v in pairs(countries_easy) do table.insert(deck, v) end
+  end
+  -- Medium
+  if difficulty > 1 then
+    print("Adding medium countries")
+    for k,v in pairs(countries_medium) do table.insert(deck, v) end
+  end
+  -- Hard
+  if difficulty > 2 then
+    print("Adding hard countries")
+    for k,v in pairs(countries_hard) do table.insert(deck, v) end
   end
 end
 
@@ -254,4 +575,4 @@ function leftInDeck()
   return count
 end
 
-return Countries
+return countries_easy
